7a791a2d7a1a08f9175d1e9645b4b19900ee6103
Dylan Martin <dylanrossmartin@gmail.com>
2024-04-03 15:13:23 -0400

    add

--- entry_edge.py
+++ entry_edge.py
@@ -1,27 +1,28 @@
 import subprocess
 import time
 import psutil
-import sys  # Ensure sys is imported for sys.exit()
+import sys
 
-# Start the shell script
+
 print("Starting the shell script...")
 subprocess.Popen(["/bin/bash", "/workspace/runKit/startup/start.sh"])
 
-# Wait a few seconds to give the nvflare process time to start
-time.sleep(10)  # Adjust this delay as needed
+
+time.sleep(10)
 
 print("Polling for nvflare process and printing process details for debugging...")
 while True:
     process_found = False
     for proc in psutil.process_iter(attrs=['cmdline']):
-        cmdline = ' '.join(proc.info['cmdline']) if proc.info['cmdline'] else ''
+        cmdline = ' '.join(proc.info['cmdline']
+                           ) if proc.info['cmdline'] else ''
         if 'nvflare' in cmdline:
             process_found = True
             print("nvflare process is running...")
-            break  # Exit the loop if nvflare process is found
-    
+            break
+
     if process_found:
-        time.sleep(10)  # Check every 10 seconds
+        time.sleep(10)
     else:
         print("nvflare process is not running anymore or not found. Exiting.")
-        sys.exit(0)  # Exit the script
+        sys.exit(0)
