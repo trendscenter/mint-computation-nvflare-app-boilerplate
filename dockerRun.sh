b571bf1b4f9c4fd5b15408cb2ca4b8347bdd16e6
Dylan Martin <dylanrossmartin@gmail.com>
2024-11-15 13:18:12 -0500

    dockerRun.sh handle spaces in path

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,6 +1,18 @@
-docker run --rm -it \
+#!/bin/bash
+
+# Get the absolute path of the script's location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LOCAL_WORKSPACE="$SCRIPT_DIR"
+REMOTE_WORKSPACE='/workspace'
+
+echo "Using LOCAL_WORKSPACE: $LOCAL_WORKSPACE"
+echo "Using REMOTE_WORKSPACE: $REMOTE_WORKSPACE"
+
+
+# Run Docker, disabling path conversion on Windows
+echo MSYS_NO_PATHCONV=1 docker run --rm -it \
     --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     --name flare \
-    -v /$(pwd):/workspace \
-    -w //workspace \
+    -v "$LOCAL_WORKSPACE:$REMOTE_WORKSPACE" \
+    -w "$REMOTE_WORKSPACE" \
     nvflare-pt:latest
